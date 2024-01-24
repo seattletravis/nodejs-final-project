375367a331e31cc689e341ab0840a860bb7e2c5f
Travis Lamberte <travis@lamberte.com>
2024-01-24 14:13:09 -0800

    added get book by title

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -37,8 +37,16 @@ public_users.get('/author/:author', function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title', function (req, res) {
-	//Write your code here
-	return res.status(300).json({ message: 'Yet to be implemented' });
+	const title = req.params.title;
+	const keys = [];
+	for (const [key, value] of Object.entries(books)) {
+		keys.push(key);
+	}
+	keys.forEach((el) => {
+		if (title === books[el].title) {
+			res.send(books[[el]]);
+		}
+	});
 });
 
 //  Get book review
