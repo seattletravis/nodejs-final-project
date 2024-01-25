84aa3fba50e810a04120670d4480bdebc9312ba4
Travis Lamberte <travis@lamberte.com>
2024-01-25 09:01:58 -0800

    revisions

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
+const session = require('express-session');
+
 let books = require('./booksdb.js');
 const { send } = require('vite');
-let isValid = require('./auth_users.js').isValid;
-let users = require('./auth_users.js').users;
+// let isValid = require('./auth_users.js').isValid;
+// let users = require('./auth_users.js').users;
+// let authenticatedUser = require('./auth_users.js').authenticatedUser;
+
 const public_users = express.Router();
 
-const doesExist = (username) => {
+let users = [];
+
+const isValid = (username) => {
 	let userswithsamename = users.filter((user) => {
 		return user.username === username;
 	});
@@ -16,11 +23,56 @@ const doesExist = (username) => {
 	}
 };
 
+const authenticatedUser = (username, password) => {
+	let validusers = users.filter((user) => {
+		return user.username === username && user.password === password;
+	});
+	if (validusers.length > 0) {
+		return true;
+	} else {
+		return false;
+	}
+};
+
+public_users.use(
+	session({ secret: 'fingerpint' }, (resave = true), (saveUninitialized = true))
+);
+
+//only registered users can login
+public_users.post('/login', (req, res) => {
+	const username = req.body.username;
+	const password = req.body.password;
+
+	if (!username || !password) {
+		return res.status(404).json({ message: 'Error logging in' });
+	}
+
+	if (authenticatedUser(username, password)) {
+		let accessToken = jwt.sign(
+			{
+				data: password,
+			},
+			'access',
+			{ expiresIn: 60 * 60 }
+		);
+
+		req.session.authorization = {
+			accessToken,
+			username,
+		};
+		return res.status(200).send('User successfully logged in');
+	} else {
+		return res
+			.status(208)
+			.json({ message: 'Invalid Login. Check username and password' });
+	}
+});
+
 public_users.post('/register', (req, res) => {
 	const username = req.body.username;
 	const password = req.body.password;
 	if (username && password) {
-		if (!doesExist(username)) {
+		if (!isValid(username)) {
 			users.push({ username: username, password: password });
 			return res
 				.status(200)
