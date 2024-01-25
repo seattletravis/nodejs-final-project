13b0374d6ab613e752baec09300b09fba8f9b396
Travis Lamberte <travis@lamberte.com>
2024-01-25 07:10:35 -0800

    fix typo in register message

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -24,7 +24,7 @@ public_users.post('/register', (req, res) => {
 			users.push({ username: username, password: password });
 			return res
 				.status(200)
-				.json({ message: 'User successfully registred. Now you can login' });
+				.json({ message: 'User successfully registered. You can now login' });
 		} else {
 			return res.status(404).json({ message: 'User already exists!' });
 		}
